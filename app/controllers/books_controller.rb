27d70370891e81530900edf61b81a7e27e9d628a
elijah.brunsvold@gmail.com <elijah.brunsvold@gmail.com>
2018-11-19 20:07:39 -0600

    Added ability to create books and they show up on the index page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,24 @@
 class BooksController < ApplicationController
+
+    def index
+        @books = Book.all.order("created_at DESC")
+    end
+
+    def new
+        @book = Book.new
+    end
+
+    def create
+        @book = Book.new(book_params)
+
+        if @book.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
+    private def book_params
+        params.require(:book).permit(:title, :description, :author)
+    end
 end
